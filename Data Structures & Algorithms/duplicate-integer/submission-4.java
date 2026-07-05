439465b6a881c3ebe73778efff74163842a9bdf7
Namita gupta <133769899+namitagupta04@users.noreply.github.com>
2026-07-05 16:21:33 +0530

    Add: duplicate-integer - submission-4

---
+++ Data Structures & Algorithms/duplicate-integer/submission-4.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+     HashSet <Integer> a = new HashSet<>();
+    for(int num:nums){
+        if(a.contains(num)){
+        return true;
+        
+    }
+     a.add(num);
+    }
+    return false;
+    }
+}
