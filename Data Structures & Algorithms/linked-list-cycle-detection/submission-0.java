093f5e349a4816f7a203d790efc8d51f1b44f2e3
Namita gupta <133769899+namitagupta04@users.noreply.github.com>
2026-07-21 06:34:55 +0530

    Add: linked-list-cycle-detection - submission-0

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slowPtr = head;
+        ListNode fastPtr = head;
+
+        while (fastPtr != null && fastPtr.next != null) {
+
+            slowPtr = slowPtr.next;
+            fastPtr = fastPtr.next.next;
+
+            if (slowPtr == fastPtr) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
