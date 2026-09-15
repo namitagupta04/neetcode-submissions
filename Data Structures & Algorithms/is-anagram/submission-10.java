6c12ff21694b978144e6923ab73e7b28ac67b18c
Namita gupta <133769899+namitagupta04@users.noreply.github.com>
2026-09-16 01:41:36 +0530

    Add: is-anagram - submission-10

---
+++ Data Structures & Algorithms/is-anagram/submission-10.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+String str1 = s.toLowerCase();
+String str2 = t.toLowerCase();
+
+  if(str1.length() != str2.length()){
+    return false;
+  }
+
+  int counts[] = new int[26];
+
+  for(int i = 0;i<str1.length();i++){
+    counts[str1.charAt(i)-'a']++;
+  }
+  for(int i = 0;i<str2.length();i++){
+    counts[str2.charAt(i)-'a']--;
+  }
+
+for(int count:counts){
+  if(count != 0){
+    return false;
+  }
+}
+  return true;
+
+  
+
+}
+}
