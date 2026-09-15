484d24c56fe2b0035a3eff200ecf95b9b518845c
Namita gupta <133769899+namitagupta04@users.noreply.github.com>
2026-09-15 14:11:30 +0530

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public boolean isAnagram(String str1, String str2) {
+
+        // Convert both to lowercase
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
+        // Remove all spaces
+        str1 = str1.replace(" ", "");
+        str2 = str2.replace(" ", "");
+
+        // Initialize the bucket array
+        int[] counts = new int[26];
+
+        // Fill the buckets using str1
+        for (int i = 0; i < str1.length(); i++) {
+            counts[str1.charAt(i) - 'a']++;
+        }
+
+        // Empty the buckets using str2
+        for (int i = 0; i < str2.length(); i++) {
+            counts[str2.charAt(i) - 'a']--;
+        }
+
+        // Check if all buckets are empty
+        for (int count : counts) {
+            if (count != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
